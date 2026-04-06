ff1dbba890622dd6b4baf1c4b3480d1e229719ce
AlᎥce <100071513+rat1alisa@users.noreply.github.com>
2026-04-06 20:10:18 +0300

    gemr

--- scripts/codee.js
+++ scripts/codee.js
@@ -1,9 +1,29 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-ctx.shadowBlur = 10;         
-ctx.shadowColor = '#679ef5';   
-ctx.fillStyle = "#679ef5";
-ctx.beginPath();
-ctx.arc(700, 950, 800, 0, Math.PI*2 ,true);
-ctx.closePath();
-ctx.fill();
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
+// ctx.shadowBlur = 10;         
+// ctx.shadowColor = '#679ef5';   
+// ctx.fillStyle = "#679ef5";
+// ctx.beginPath();
+// ctx.arc(700, 950, 800, 0, Math.PI*2 ,true);
+// ctx.closePath();
+// ctx.fill();
+
+
+document.addEventListener('click', function(e) {
+    
+    const rect = element.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const xPos = x - 155;
+    const yPos = y - 95;
+    let img = document.createElement('img');
+    img.src = 'styles/imgs/krest.png';
+    img.style.position = 'absolute';
+    img = document.querySelector('.figr');
+
+    // Позиция картинки там, где кликнули
+    img.style.left = xPos + 'px'; 
+    img.style.top = yPos + 'px';
+    document.body.appendChild(img);
+    console.log("ХУЙ");
+  });
--- scripts/dist/codee.dev.js
+++ scripts/dist/codee.dev.js
@@ -1,12 +1,28 @@
 "use strict";
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-ctx.shadowBlur = 10;
-ctx.shadowColor = '#679ef5';
-ctx.fillStyle = "#679ef5";
-ctx.beginPath();
-ctx.arc(700, 950, 800, 0, Math.PI * 2, true);
-ctx.closePath();
-ctx.fill();
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
+// ctx.shadowBlur = 10;         
+// ctx.shadowColor = '#679ef5';   
+// ctx.fillStyle = "#679ef5";
+// ctx.beginPath();
+// ctx.arc(700, 950, 800, 0, Math.PI*2 ,true);
+// ctx.closePath();
+// ctx.fill();
+document.addEventListener('click', function (e) {
+  var rect = element.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+  var xPos = x - 155;
+  var yPos = y - 95;
+  var img = document.createElement('img');
+  img.src = 'styles/imgs/krest.png';
+  img.style.position = 'absolute';
+  img = document.querySelector('.figr'); // Позиция картинки там, где кликнули
+
+  img.style.left = xPos + 'px';
+  img.style.top = yPos + 'px';
+  document.body.appendChild(img);
+  console.log("ХУЙ");
+});
 //# sourceMappingURL=codee.dev.js.map
